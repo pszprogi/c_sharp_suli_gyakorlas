295a9282a49ccb60ddb53c57cbb7d42fd77c154b
pszprogi <pszprogi@gmail.com>
2024-04-12 16:37:12 +0200

    elso feltoltes

---
+++ Ember/Ember/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ember
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+
+        public Person() { }
+
+        public Person(string name, int age) 
+        {
+            this.name = name;
+            this.age = age; 
+        
+        
+        }
+
+        public void WriteInformation() 
+        {
+            Console.WriteLine($"Név: {name}, Kor: {age}.");
+        
+        
+        
+        
+        
+        
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Ember/Ember/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Jozsi = new Person("Józsi", 40);
+            Person Pista = new Person("Pista", 35);
+
+            Jozsi.WriteInformation();
+            Pista.WriteInformation();
+
+
+
+        }
+    }
+}
