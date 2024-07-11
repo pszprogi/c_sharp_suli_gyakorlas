6602e07e32cd9bf99821ae52c0f244a125100ec9
pszprogi <pszprogi@gmail.com>
2024-07-11 16:46:40 +0200

    laptop 2024.07.11

---
+++ Gyakorlas_2024_07_11/Gyakorlas_2024_07_11/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Gyakorlas_2024_07_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy szöveget, ami szám.");
+            //string szam_ami_szoveg = Console.ReadLine();
+
+            int eredmeny = 100;
+           
+            if (!int.TryParse(Console.ReadLine(), out eredmeny))
+            {
+                Console.WriteLine("Nem szám.");
+
+
+
+
+
+            }
+
+            Console.WriteLine(eredmeny);
+
+
+
+
+
+
+        }
+    }
+}
